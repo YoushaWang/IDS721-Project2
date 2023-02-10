0fa1df6086bb13f2d61bc0cb0f417bafdbe34ee6
YoushaWang <122926209+YoushaWang@users.noreply.github.com>
2023-02-10 03:48:09 +0000

    revise app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "Hello, this is my project 2"
+    return "Hello, this is my project 2"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
